ab8248b5668ae53728fd357b545dc51d0b376555
Geesung <gsethan17@gamil.com>
2021-06-13 19:39:37 +0900

    add sequenctial data and model load

--- generate_dataset.py
+++ generate_dataset.py
@@ -11,34 +11,7 @@ PATH_SWITCH_INFO = os.path.join(os.getcwd(), 'data_guide', 'dropDetectError')
 PATH_DATA = '/home/gsethan/Documents/Aff-Wild2-ICCV2021/'
 # PATH_DATA = os.path.join(os.getcwd(), 'data')
 
-
-list_subjects = sorted([x.rstrip('.csv') for x in os.listdir(PATH_DATA_GUIDE) if x.endswith('.csv')])
-
-
-
-total_samples = {}
-
-for i, name in enumerate(list_subjects):
-    file_path = os.path.join(PATH_DATA_GUIDE, name + '.csv')
-
-    total_samples[name] = read_csv(file_path)
-
-
-
-list_trains = read_csv(os.path.join(PATH_DATA, 'va_train_set.csv'))
-list_vals = read_csv(os.path.join(PATH_DATA, 'va_val_set.csv'))
-
-
-
-dic_trains = {}
-for subject in list_trains:
-    if subject in total_samples.keys():
-        dic_trains[subject] = copy.deepcopy(total_samples[subject])
-
-dic_vals = {}
-for subject in list_vals:
-    if subject in total_samples.keys():
-        dic_vals[subject] = copy.deepcopy(total_samples[subject])
+FPS = 30
 
 def count(dic):
     count = 0
@@ -50,16 +23,7 @@ def count(dic):
 
     return count-count_na
 
-dic_trains_count = count(dic_trains)
-dic_vals_count = count(dic_vals)
-
-print(dic_trains_count)
-print(dic_vals_count)
-
-switch_images = read_pickle(os.path.join(PATH_SWITCH_INFO, 'switch_images.pickle'))
-switch_subjects = read_pickle(os.path.join(PATH_SWITCH_INFO, 'switch_subjects.pickle'))
-
-def get_samples(dic) :
+def get_samples(dic, switch_images, switch_subjects) :
     list_x = []
     list_y = []
 
@@ -103,36 +67,177 @@ def get_samples(dic) :
     print(error_list)
     return list_x, list_y
 
+def generate_single_data() :
+    list_subjects = sorted([x.rstrip('.csv') for x in os.listdir(PATH_DATA_GUIDE) if x.endswith('.csv')])
 
-print('Training data')
-train_x_list, train_y_list = get_samples(dic_trains)
-print(len(train_x_list))
-print(len(train_y_list))
+    total_samples = {}
 
-train_data = {
-    'x' : train_x_list,
-    'y' : train_y_list
-}
+    for i, name in enumerate(list_subjects):
+        file_path = os.path.join(PATH_DATA_GUIDE, name + '.csv')
 
-train_save_path = os.path.join(PATH_DATA, 'va_train_list.pickle')
-
-with open(train_save_path, 'wb') as f :
-    pickle.dump(train_data, f)
+        total_samples[name] = read_csv(file_path)
 
 
-print('Validation data')
 
-val_x_list, val_y_list = get_samples(dic_vals)
-print(len(val_x_list))
-print(len(val_y_list))
+    list_trains = read_csv(os.path.join(PATH_DATA, 'va_train_set.csv'))
+    list_vals = read_csv(os.path.join(PATH_DATA, 'va_val_set.csv'))
 
-val_data = {
-    'x' : val_x_list,
-    'y' : val_y_list
-}
 
-val_save_path = os.path.join(PATH_DATA, 'va_val_list.pickle')
 
-with open(val_save_path, 'wb') as f :
-    pickle.dump(val_data, f)
+    dic_trains = {}
+    for subject in list_trains:
+        if subject in total_samples.keys():
+            dic_trains[subject] = copy.deepcopy(total_samples[subject])
 
+    dic_vals = {}
+    for subject in list_vals:
+        if subject in total_samples.keys():
+            dic_vals[subject] = copy.deepcopy(total_samples[subject])
+
+
+    dic_trains_count = count(dic_trains)
+    dic_vals_count = count(dic_vals)
+
+    print(dic_trains_count)
+    print(dic_vals_count)
+
+    switch_images = read_pickle(os.path.join(PATH_SWITCH_INFO, 'switch_images.pickle'))
+    switch_subjects = read_pickle(os.path.join(PATH_SWITCH_INFO, 'switch_subjects.pickle'))
+
+    print('Training data')
+    train_x_list, train_y_list = get_samples(dic_trains, switch_images, switch_subjects)
+    print(len(train_x_list))
+    print(len(train_y_list))
+
+    train_data = {
+        'x' : train_x_list,
+        'y' : train_y_list
+    }
+
+    train_save_path = os.path.join(PATH_DATA, 'va_train_list.pickle')
+
+    with open(train_save_path, 'wb') as f :
+        pickle.dump(train_data, f)
+
+
+    print('Validation data')
+
+    val_x_list, val_y_list = get_samples(dic_vals, switch_images, switch_subjects)
+    print(len(val_x_list))
+    print(len(val_y_list))
+
+    val_data = {
+        'x' : val_x_list,
+        'y' : val_y_list
+    }
+
+    val_save_path = os.path.join(PATH_DATA, 'va_val_list.pickle')
+
+    with open(val_save_path, 'wb') as f :
+        pickle.dump(val_data, f)
+
+def get_sequence_data(subject_name, images_list, window_size, stride) :
+    total_x = []
+    total_y = []
+
+    for i in range(len(images_list)) :
+        if i >= FPS * window_size :
+
+            # get images
+            list_x = []
+            for j in range((FPS*window_size // stride)+1) :
+
+                idx = i-(stride * j)
+
+                if images_list[idx] is '' :
+
+                    if idx == 0 :
+                        list_x.append(os.path.join(subject_name, images_list[idx] + '.jpg'))
+
+                    else :
+                        for k in range(idx-1, idx-stride, -1) :
+                            flag = False
+
+                            if not images_list[k] is '' :
+                                list_x.append(os.path.join(subject_name, images_list[k] + '.jpg'))
+                                flag = True
+                                break
+
+                        if not flag :
+                            list_x.append(os.path.join(subject_name, images_list[idx] + '.jpg'))
+
+                else :
+                    list_x.append(os.path.join(subject_name, images_list[idx] + '.jpg'))
+
+            list_x.reverse()
+            total_x.append(list_x)
+
+            # get labels
+            path = os.path.join(PATH_DATA, 'annotations', 'VA_Set', '**', subject_name + '.txt')
+            path = glob.glob(path)[0]
+
+            list_labels = read_txt(path)
+            try :
+                total_y.append([float(x) for x in list_labels[(i + 1)]])
+            except :
+                print(subject_name, i)
+
+    return total_x, total_y
+
+
+def generate_sequential_data(window_size, stride) :
+    # Train data
+    train_data = {
+        'x': [],
+        'y': []
+    }
+
+    train_subject_lists = read_csv(os.path.join(PATH_DATA, 'va_train_set.csv'))
+    for train_subject_list in train_subject_lists :
+        train_images = read_csv(os.path.join(PATH_DATA_GUIDE, train_subject_list+'.csv'))
+
+        train_x, train_y = get_sequence_data(train_subject_list, train_images, window_size, stride)
+
+        train_data['x'] += train_x
+        train_data['y'] += train_y
+
+        print(np.array(train_data['x']).shape, np.array(train_data['y']).shape, np.array(train_x).shape)
+
+    print(np.array(train_data['x']).shape, np.array(train_data['y']).shape)
+    if np.array(train_data['x']).shape[0] == np.array(train_data['y']).shape[0] :
+        with open(os.path.join(PATH_DATA, 'va_train_seq_list.pickle'), 'wb') as f:
+            pickle.dump(train_data, f)
+
+
+    # Validation data
+    val_data = {
+        'x': [],
+        'y': []
+    }
+
+    val_subject_lists = read_csv(os.path.join(PATH_DATA, 'va_val_set.csv'))
+    for val_subject_list in val_subject_lists:
+        val_images = read_csv(os.path.join(PATH_DATA_GUIDE, val_subject_list + '.csv'))
+
+        val_x, val_y = get_sequence_data(val_subject_list, val_images, window_size, stride)
+
+        val_data['x'] += val_x
+        val_data['y'] += val_y
+
+        print(np.array(val_data['x']).shape, np.array(val_data['y']).shape, np.array(val_x).shape)
+
+    print(np.array(val_data['x']).shape, np.array(val_data['y']).shape)
+    if np.array(val_data['x']).shape[0] == np.array(val_data['y']).shape[0] :
+        with open(os.path.join(PATH_DATA, 'va_val_seq_list.pickle'), 'wb') as f:
+            pickle.dump(val_data, f)
+
+
+
+
+if __name__ == "__main__" :
+    # generate_single_data()
+
+    window_size = 3
+    stride = 10
+
+    generate_sequential_data(window_size, stride)
--- squ_test.py
+++ squ_test.py
@@ -1,7 +1,7 @@
 from utils import get_model
 import tensorflow as tf
 
-MODEL_KEY = 'resnet50_gru'  # 'FER' / 'FER_LSTM' / 'resnet50' / 'resnet50_gru'
+MODEL_KEY = 'vgg19_gru'  # 'FER' / 'FER_LSTM' / 'resnet50' / 'resnet50_gru' / 'vgg19_gru'
 PRETRAINED = True
 WINDOW_SIZE = 10
 
@@ -11,8 +11,8 @@ MODEL = get_model(key=MODEL_KEY, preTrained=PRETRAINED, window_size=WINDOW_SIZE)
 
 print(MODEL.summary())
 
-input_ = tf.ones((1, 10, 112, 112, 3))
+# input_ = tf.ones((1, 10, 112, 112, 3))
 
-output_ = MODEL.predict(input_)
-
-print(output_)
+# output_ = MODEL.predict(input_)
+#
+# print(output_)

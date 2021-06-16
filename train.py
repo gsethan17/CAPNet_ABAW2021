2a4c380a229181d212a743427599fe99b042cccf
Geesung <gsethan17@gamil.com>
2021-06-16 18:28:23 +0900

    add pre-evaluation

--- train.py
+++ train.py
@@ -140,6 +140,19 @@ def main() :
     print("Validation data has been build ({:.1f}seconds).".format(ed_val - ed_train))
 
 
+    # pre-evaluation
+    print("Pre-evaluation Start...")
+    eval_result = []
+
+    for e in range(BATCH_SIZE*100) :
+        eval_x, eval_y = val_dataloader[e]
+
+        _, eval_metric = val_step(eval_x, eval_y)
+        eval_result.append(tf.math.reduce_mean(eval_metric).numpy())
+        print("{:>5} / {:>5} || {:.4f}".format(i + 1, BATCH_SIZE*100, sum(eval_result)/len(eval_result)), end='\r')
+    print("Pre-evaluation result : ", sum(eval_result)/len(eval_result))
+    val_dataloader.on_epoch_end()
+
     ## use gradient tape
     results = {}
     results['train_loss'] = []
@@ -154,9 +167,6 @@ def main() :
 
     print("Training Start...")
     for epoch in range(EPOCHS) :
-        if LEARNING_RATE_DECAY:
-            OPTIMIZER = Adam(learning_rate=(LEARNING_RATE - (epoch * DECAY_CONSTANT)))
-
         train_loss = []
         train_metric_V = []
         train_metric_A = []
@@ -168,17 +178,18 @@ def main() :
             x_train, y_train = train_dataloader[i]
             print("Training : {} / {}".format(i + 1, len(train_dataloader)), end="\r")
             train_temp_loss, train_temp_metric = train_step(x_train, y_train)
-            train_loss.append(train_temp_loss)
-            train_metric_V.append(train_temp_metric[0])
-            train_metric_A.append(train_temp_metric[1])
-            train_metric_C.append(tf.math.reduce_mean(train_temp_metric))
+            train_loss.append(train_temp_loss.numpy())
+            train_metric_V.append(train_temp_metric[0].numpy())
+            train_metric_A.append(train_temp_metric[1].numpy())
+            train_metric_C.append(tf.math.reduce_mean(train_temp_metric).numpy())
+        train_dataloader.on_epoch_end()
         ed_train = time.time()
         print("Training is completed...", end="\r")
 
-        results['train_loss'].append(tf.math.reduce_mean(train_loss).numpy())
-        results['train_ccc_V'].append(tf.math.reduce_mean(train_metric_V).numpy())
-        results['train_ccc_A'].append(tf.math.reduce_mean(train_metric_A).numpy())
-        results['train_CCC'].append(tf.math.reduce_mean(train_metric_C).numpy())
+        results['train_loss'].append(sum(train_loss)/len(train_loss))
+        results['train_ccc_V'].append(sum(train_metric_V)/len(train_metric_V))
+        results['train_ccc_A'].append(sum(train_metric_A)/len(train_metric_A))
+        results['train_CCC'].append(sum(train_metric_C)/len(train_metric_C))
 
         val_loss = []
         val_metric_V = []
@@ -191,23 +202,25 @@ def main() :
             x_val, y_val = val_dataloader[j]
             print("Validation : {} / {}".format(j + 1, len(val_dataloader)), end="\r")
             val_temp_loss, val_temp_metric = val_step(x_val, y_val)
-            val_loss.append(val_temp_loss)
-            val_metric_V.append(val_temp_metric[0])
-            val_metric_A.append(val_temp_metric[1])
-            val_metric_C.append(tf.math.reduce_mean(val_temp_metric))
+            val_loss.append(val_temp_loss.numpy())
+            val_metric_V.append(val_temp_metric[0].numpy())
+            val_metric_A.append(val_temp_metric[1].numpy())
+            val_metric_C.append(tf.math.reduce_mean(val_temp_metric).numpy())
+        val_dataloader.on_epoch_end()
 
         ed_val = time.time()
 
         MODEL.save_weights(os.path.join(SAVE_PATH, "{}epoch_weights".format(epoch+1)))
 
-        if tf.math.reduce_mean(val_metric_C).numpy() > tf.math.reduce_max(results['val_CCC']).numpy() :
-            # save best weights
-            MODEL.save_weights(os.path.join(SAVE_PATH, "best_weights"))
+        if epoch > 0 :
+            if (sum(val_metric_C) / len(val_metric_C)) > max(results['val_CCC']) :
+                # save best weights
+                MODEL.save_weights(os.path.join(SAVE_PATH, "best_weights"))
 
-        results['val_loss'].append(tf.math.reduce_mean(val_loss).numpy())
-        results['val_ccc_V'].append(tf.math.reduce_mean(val_metric_V).numpy())
-        results['val_ccc_A'].append(tf.math.reduce_mean(val_metric_A).numpy())
-        results['val_CCC'].append(tf.math.reduce_mean(val_metric_C).numpy())
+        results['val_loss'].append(sum(val_loss) / len(val_loss))
+        results['val_ccc_V'].append(sum(val_metric_V) / len(val_metric_V))
+        results['val_ccc_A'].append(sum(val_metric_A) / len(val_metric_A))
+        results['val_CCC'].append(sum(val_metric_C) / len(val_metric_C))
 
         print("{:>3} / {:>3} || train_loss:{:8.4f}, train_CCC:{:8.4f}, val_loss:{:8.4f}, val_CCC:{:8.4f} || TIME: Train {:8.1f}sec, Validation {:8.1f}sec".format(epoch + 1, EPOCHS,
                                                                                       results['train_loss'][-1],
@@ -219,7 +232,7 @@ def main() :
 
         # early stop
         if epoch > 5 :
-            if results['val_CCC'][-5] > tf.math.reduce_max(results['val_CCC'][-4:]) :
+            if results['val_CCC'][-5] > max(results['val_CCC'][-4:]) :
                 break
 
         df = pd.DataFrame(results)

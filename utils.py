29ae66484204a0913496e9ff9cb61afb503f216b
Geesung <gsethan17@gamil.com>
2021-06-20 01:05:26 +0900

    add td train

--- utils.py
+++ utils.py
@@ -263,30 +263,23 @@ def reshape(img, image_size) :
     return img
 
 def load_td_image(images, image_size) :
-    if os.path.isfile(images[0]) * os.path.isfile(images[0]) * os.path.isfile(images[0]) :
-        try :
-            img1 = cv2.imread(images[0])
-            img2 = cv2.imread(images[1])
-            img3 = cv2.imread(images[2])
+    if os.path.isfile(images[0]) * os.path.isfile(images[1]) * os.path.isfile(images[2]) :
+        img1 = cv2.imread(images[0])
+        img2 = cv2.imread(images[1])
+        img3 = cv2.imread(images[2])
 
-            img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-            img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-            img3_gray = cv2.cvtColor(img3, cv2.COLOR_BGR2GRAY)
+        img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+        img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+        img3_gray = cv2.cvtColor(img3, cv2.COLOR_BGR2GRAY)
 
-            _, diff1 = ssim(img1_gray, img2_gray, full=True)
-            _, diff2 = ssim(img1_gray, img3_gray, full=True)
+        _, diff1 = ssim(img1_gray, img2_gray, full=True)
+        _, diff2 = ssim(img1_gray, img3_gray, full=True)
 
-            base = reshape(img1_gray, image_size)
-            diff1 = reshape(diff1*-1, image_size)
-            diff2 = reshape(diff2*-1, image_size)
+        base = reshape(img1_gray, image_size)
+        diff1 = reshape(diff1*-1, image_size)
+        diff2 = reshape(diff2*-1, image_size)
 
-            image_x = tf.concat([base, diff1, diff2], axis = -1)
-
-        except :
-            print(images)
-            print(os.path.isfile(images[0]))
-            print(os.path.isfile(images[1]))
-            print(os.path.isfile(images[2]))
+        image_x = tf.concat([base, diff1, diff2], axis = -1)
 
         return True, image_x
 

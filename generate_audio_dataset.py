3bef7f86ddeb8a2487b5ccf960a65aac311a7f9c
Geesung <gsethan17@gamil.com>
2021-06-19 20:16:03 +0900

    add video maker

--- generate_audio_dataset.py
+++ generate_audio_dataset.py
@@ -1,11 +1,12 @@
 import moviepy.editor as mp
 import os
 
-PATH_DATA = '/home/gsethan/Documents/Aff-Wild2-ICCV2021/'
+# PATH_DATA = '/home/gsethan/Documents/Aff-Wild2-ICCV2021/'
+PATH_DATA = 'D:\\Git\\ABAW2021\\data\\'
 PATH_VIDEO = os.path.join(PATH_DATA, 'videos')
-PATH_AUDIO = os.path.join(PATH_DATA, 'audios')
-if not os.path.isdir(PATH_AUDIO) :
-    os.makedirs(PATH_AUDIO)
+# PATH_AUDIO = os.path.join(PATH_DATA, 'audios')
+# if not os.path.isdir(PATH_AUDIO) :
+#     os.makedirs(PATH_AUDIO)
 
 def generate_audio_file() :
     global PATH_VIDEO
@@ -23,5 +24,17 @@ def generate_audio_file() :
 
 
 if __name__ == "__main__" :
-    generate_audio_file()
+    # generate_audio_file()
+
+    audiosource = os.path.join(PATH_VIDEO, '5-60-1920x1080-4.mp4')
+    videosource = os.path.join(PATH_VIDEO, '5-60-1920x1080-4_predict.mp4')
+    output = os.path.join(PATH_VIDEO, '5-60-1920x1080-4_predict_withsound.mp4')
+
+    videoclip = mp.VideoFileClip(videosource)
+    audioclip = mp.VideoFileClip(audiosource)
+    audioclip_get = audioclip.audio
+
+    videoclip.audio = audioclip_get
+
+    videoclip.write_videofile(output)
 

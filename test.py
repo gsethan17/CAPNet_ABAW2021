0fe87142e8ccd3cd0f51f80d327f111f2ed8cd82
Geesung <gsethan17@gamil.com>
2021-06-22 19:14:31 +0900

    add test code

---
+++ test.py
@@ -0,0 +1,16 @@
+from utils import get_model, metric_CCC, read_pickle, Dataloader, read_csv, load_image, read_txt, CCC_score_np
+import tensorflow as tf
+
+
+MODEL = get_model(key='FER_LSTM', preTrained=True,
+                  weight_path='D:\\Git\ABAW2021\\results\\620_17_33_FER_LSTM\\best_weights',
+                  input_size = (224, 224),
+                  dropout_rate=0.2)
+
+input_ = tf.ones((1, 10, 224, 224, 3))
+
+output_ = MODEL(input_)
+output_pred = MODEL.predict(input_)
+
+print(output_)
+print(output_pred)

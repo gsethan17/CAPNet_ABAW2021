7d75de65d121dec232192bbb75d8caba4ab113f9
Geesung <gsethan17@gamil.com>
2021-06-22 12:22:17 +0900

    add merge

--- utils.py
+++ utils.py
@@ -520,8 +520,8 @@ def merge(path1, path2) :
         f.write(content)
 
         for j in range(len(prediction1) - 1):
-            cur = [float(x) for x in prediction1[(i + 1)]]
-            ref = [float(x) for x in prediction2[(i + 1)]]
+            cur = [float(x) for x in prediction1[(j + 1)]]
+            ref = [float(x) for x in prediction2[(j + 1)]]
             print(cur)
             print(ref)
             print("")

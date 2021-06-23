40e5902cd7a11a6b84550d720d45b91f5ef6b58d
Geesung <gsethan17@gamil.com>
2021-06-23 16:04:24 +0900

    update

--- generate_audio_dataset.py
+++ generate_audio_dataset.py
@@ -4,28 +4,35 @@ import os
 # PATH_DATA = '/home/gsethan/Documents/Aff-Wild2-ICCV2021/'
 PATH_DATA = 'D:\\Git\\ABAW2021\\data\\'
 PATH_VIDEO = os.path.join(PATH_DATA, 'videos')
-# PATH_AUDIO = os.path.join(PATH_DATA, 'audios')
-# if not os.path.isdir(PATH_AUDIO) :
-#     os.makedirs(PATH_AUDIO)
+PATH_AUDIO = os.path.join(PATH_DATA, 'audios')
+if not os.path.isdir(PATH_AUDIO) :
+    os.makedirs(PATH_AUDIO)
 
 def generate_audio_file() :
     global PATH_VIDEO
     global PATH_AUDIO
 
     for name_video in os.listdir(PATH_VIDEO) :
-        PATH_SAVE_AUDIO = os.path.join(PATH_AUDIO, name_video.split('.')[0] + '.mp3')
+        PATH_SAVE_AUDIO = os.path.join(PATH_AUDIO, name_video.split('.')[0] + '.wav')
 
         if not os.path.isfile(PATH_SAVE_AUDIO) :
-            videoclip = mp.VideoFileClip(os.path.join(PATH_VIDEO, name_video))
-            videoclip.audio.write_audiofile(PATH_SAVE_AUDIO)
+            if os.path.isfile(os.path.join(PATH_VIDEO, name_video)) :
+                try :
+                    videoclip = mp.VideoFileClip(os.path.join(PATH_VIDEO, name_video))
+                    videoclip.audio.write_audiofile(PATH_SAVE_AUDIO)
+                except :
+                    print("{} has something wrong!!".format(name_video))
+            else :
+                print("Video file for '{}' is not exist".format(name_video))
 
         else :
             print("Audio file for '{}' is already exist".format(name_video))
 
 
 if __name__ == "__main__" :
-    # generate_audio_file()
+    generate_audio_file()
 
+    '''
     audiosource = os.path.join(PATH_VIDEO, '5-60-1920x1080-4.mp4')
     videosource = os.path.join(PATH_VIDEO, '5-60-1920x1080-4_predict.mp4')
     output = os.path.join(PATH_VIDEO, '5-60-1920x1080-4_predict_withsound.mp4')
@@ -37,4 +44,5 @@ if __name__ == "__main__" :
     videoclip.audio = audioclip_get
 
     videoclip.write_videofile(output)
+    '''
 

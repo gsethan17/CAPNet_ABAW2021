32c850241c721f24c09e28b515d2d03de428286d
Geesung <gsethan17@gamil.com>
2021-07-07 16:19:56 +0900

    add test date generator

--- generate_dataset.py
+++ generate_dataset.py
@@ -96,40 +96,32 @@ def filtering_invalid(dic) :
     for j in range(lists) :
         keys = dic.keys()
         for key in keys :
-            dic[key].pop(i)
+            dic[key].pop(j)
 
     return dic
 
-# def generate_single_test() :
-#     base_dir = os.path.join(PATH_DATA, 'test_images_for_demo')
-#     if not os.path.isdir(base_dir) :
-#         print("You need the image, please download the 'test_images_for_demo'.")
-#         return -1
-#
-#     list_tests = read_csv(os.path.join(PATH_DATA, 'va_test_set.csv'))
-#
-#     for name in list_tests :
-#
-#         if "_" in name :
-#             if name.split('_')[-1] == 'right' or name.split('_')[-1] == 'left':
-#                 video_pos = os.path.join(PATH_DATA, 'videos', '_'.join(name.split('_')[:-1]) + '.*')
-#             else:
-#                 video_pos = os.path.join(PATH_DATA, 'videos', name + '.*')
-#         else:
-#             video_pos = os.path.join(PATH_DATA, 'videos', name + '.*')
-#
-#         if not len(glob.glob(video_pos)) == 1:
-#             print("Video path is not vaild : {}".format(name))
-#             return -1
-#
-#         cap = cv2.VideoCapture(video_pos)
-#         total_length = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+def filtering_topfull(dic) :
+    lists = []
+    for i in range(len(dic['x'])) :
+        count = 0
+        for j, image_path in enumerate(dic['x'][i]) :
+            image = image_path.split('/')[1].split('.')[0]
+            if image == "" :
+                count += 1
 
 
+        if count = (FPS * WINDOW_SIZE // STRIDE) + 1
+            lists.append(i)
 
+    print("{} invalid data is detected".format(len(lists)))
 
+    lists.reverse()
+    for j in range(lists) :
+        keys = dic.keys()
+        for key in keys :
+            dic[key].pop(j)
 
-
+    return dic
 
 
 def generate_single_train() :
@@ -214,7 +206,74 @@ def switching(name, image, switch_images, switch_subjects) :
 
     return object
 
-def get_sequence_data(subject_name, images_list, window_size, stride, switch_images, switch_subjects) :
+def get_sequence_test(subject_name) :
+    total_x = []
+    total_idx = []
+
+    if "_" in subject_name:
+        if subject_name.split('_')[-1] == 'right' or subject_name.split('_')[-1] == 'left':
+            video_pos = os.path.join(PATH_DATA, 'videos', '_'.join(subject_name.split('_')[:-1]) + '.*')
+        else:
+            video_pos = os.path.join(PATH_DATA, 'videos', subject_name + '.*')
+    else:
+        video_pos = os.path.join(PATH_DATA, 'videos', subject_name + '.*')
+
+    if not len(glob.glob(video_pos)) == 1:
+        print("Video path is not vaild : {}".format(subject_name))
+        return -1
+
+    video_path = glob.glob(video_pos)[0]
+
+    # count total number of frame
+    capture = cv2.VideoCapture(video_path)
+    total_len = capture.get(cv2.CAP_PROP_FRAME_COUNT)
+
+    base_dir = os.path.join(PATH_DATA, 'test_images_for_demo')
+
+    for i in range(total_len) :
+        if i >= FPS * WINDOW_SIZE:
+
+            # get images
+            list_x = []
+            for j in range((FPS * WINDOW_SIZE // STRIDE) + 1):
+
+                idx = i - (STRIDE * j)
+
+                image_path = os.path.join(base_dir, 'cropped', subject_name, '{:0>5}'.format(idx + 1) + '.jpg')
+
+                if not os.path.isfile(image_path) :
+                    if idx == 0 :
+                        list_x.append(os.path.join(subject_name, ""))
+                    else :
+                        for k in range(idx - 1, idx - STRIDE, -1):
+                            flag = False
+                            image_path = os.path.join(base_dir, 'cropped', subject_name,
+                                                      '{:0>5}'.format(idx + 1) + '.jpg')
+                            if os.path.isfile(image_path) :
+                                list_x.append(os.path.join(subject_name, '{:0>5}'.format(idx + 1) + '.jpg'))
+                                flag = True
+                                break
+
+                        if not flag :
+                            list_x.append(os.path.join(subject_name, ""))
+                else :
+                    list_x.append(os.path.join(subject_name, '{:0>5}'.format(idx + 1) + '.jpg'))
+
+            list_x.reverse()
+
+            # get labels & index
+            try:
+                total_x.append(list_x)
+                total_idx.append([subject_name, i])
+
+            except:
+                print(subject_name, i)
+
+    return total_x, total_idx
+
+
+
+def get_sequence_data(subject_name, images_list, switch_images, switch_subjects) :
     total_x = []
     total_y = []
     total_idx = []
@@ -224,13 +283,13 @@ def get_sequence_data(subject_name, images_list, window_size, stride, switch_ima
     list_labels = read_txt(path)
 
     for i in range(len(images_list)) :
-        if i >= FPS * window_size :
+        if i >= FPS * WINDOW_SIZE :
 
             # get images
             list_x = []
-            for j in range((FPS*window_size // stride)+1) :
+            for j in range((FPS*WINDOW_SIZE // STRIDE)+1) :
 
-                idx = i-(stride * j)
+                idx = i-(STRIDE * j)
 
                 if images_list[idx] is "" :
 
@@ -239,7 +298,7 @@ def get_sequence_data(subject_name, images_list, window_size, stride, switch_ima
                         list_x.append(os.path.join(name, images_list[idx]))
 
                     else :
-                        for k in range(idx-1, idx-stride, -1) :
+                        for k in range(idx-1, idx-STRIDE, -1) :
                             flag = False
 
                             if not images_list[k] is "" :
@@ -270,59 +329,88 @@ def get_sequence_data(subject_name, images_list, window_size, stride, switch_ima
     return total_x, total_y, total_idx
 
 
-def generate_sequential_data(window_size, stride) :
-    # read switch info
-    switch_images = read_pickle(os.path.join(PATH_SWITCH_INFO, 'switch_images.pickle'))
-    switch_subjects = read_pickle(os.path.join(PATH_SWITCH_INFO, 'switch_subjects.pickle'))
+def generate_sequential_data(type = 'test') :
+    if type == 'test' :
+        # test data
+        test_data = {
+            'x': [],
+            'i': []
+        }
 
-    # Train data
-    train_data = {
-        'x': [],
-        'y': [],
-        'i': []
-    }
+        test_subject_lists = read_csv(os.path.join(PATH_DATA, 'va_test_set.csv'))
+        for j, test_subject_list in enumerate(test_subject_lists):
 
-    train_subject_lists = read_csv(os.path.join(PATH_DATA, 'va_train_set.csv'))
-    for i, train_subject_list in enumerate(train_subject_lists) :
-        train_images = read_csv(os.path.join(PATH_DATA_GUIDE, train_subject_list+'.csv'))
+            test_x, test_idx = get_sequence_test(test_subject_list, WINDOW_SIZE, STRIDE)
 
-        train_x, train_y, train_idx = get_sequence_data(train_subject_list, train_images, window_size, stride, switch_images, switch_subjects)
+            test_data['x'] += test_x
+            test_data['i'] += test_idx
 
-        train_data['x'] += train_x
-        train_data['y'] += train_y
-        train_data['i'] += train_idx
+            print(j, len(test_subject_lists), np.array(test_data['x']).shape, np.array(test_x).shape)
 
-        print(i, len(train_subject_lists), np.array(train_data['x']).shape, np.array(train_data['y']).shape, np.array(train_x).shape)
+        test_data = filtering_topfull(test_data)
 
-    print(np.array(train_data['x']).shape, np.array(train_data['y']).shape)
-    if np.array(train_data['x']).shape[0] == np.array(train_data['y']).shape[0] :
-        with open(os.path.join(PATH_DATA, 'va_train_seq_list.pickle'), 'wb') as f:
-            pickle.dump(train_data, f)
+        if np.array(test_data['x']).shape[0] == np.array(test_data['i']).shape[0]:
+            with open(os.path.join(PATH_DATA, 'va_test_seq_list.pickle'), 'wb') as f:
+                pickle.dump(test_data, f)
 
 
-    # Validation data
-    val_data = {
-        'x': [],
-        'y': [],
-        'i': []
-    }
+    else :
+        # read switch info
+        switch_images = read_pickle(os.path.join(PATH_SWITCH_INFO, 'switch_images.pickle'))
+        switch_subjects = read_pickle(os.path.join(PATH_SWITCH_INFO, 'switch_subjects.pickle'))
 
-    val_subject_lists = read_csv(os.path.join(PATH_DATA, 'va_val_set.csv'))
-    for j, val_subject_list in enumerate(val_subject_lists):
-        val_images = read_csv(os.path.join(PATH_DATA_GUIDE, val_subject_list + '.csv'))
+        # Train data
+        train_data = {
+            'x': [],
+            'y': [],
+            'i': []
+        }
 
-        val_x, val_y, val_idx = get_sequence_data(val_subject_list, val_images, window_size, stride, switch_images, switch_subjects)
+        train_subject_lists = read_csv(os.path.join(PATH_DATA, 'va_train_set.csv'))
+        for i, train_subject_list in enumerate(train_subject_lists) :
+            train_images = read_csv(os.path.join(PATH_DATA_GUIDE, train_subject_list+'.csv'))
 
-        val_data['x'] += val_x
-        val_data['y'] += val_y
-        val_data['i'] += val_idx
+            train_x, train_y, train_idx = get_sequence_data(train_subject_list, train_images, WINDOW_SIZE, STRIDE, switch_images, switch_subjects)
 
-        print(j, len(val_subject_lists), np.array(val_data['x']).shape, np.array(val_data['y']).shape, np.array(val_x).shape)
+            train_data['x'] += train_x
+            train_data['y'] += train_y
+            train_data['i'] += train_idx
 
-    print(np.array(val_data['x']).shape, np.array(val_data['y']).shape)
-    if np.array(val_data['x']).shape[0] == np.array(val_data['y']).shape[0] :
-        with open(os.path.join(PATH_DATA, 'va_val_seq_list.pickle'), 'wb') as f:
-            pickle.dump(val_data, f)
+            print(i, len(train_subject_lists), np.array(train_data['x']).shape, np.array(train_data['y']).shape, np.array(train_x).shape)
+
+        train_data = filtering_topfull(train_data)
+
+        print(np.array(train_data['x']).shape, np.array(train_data['y']).shape)
+        if np.array(train_data['x']).shape[0] == np.array(train_data['y']).shape[0] :
+            with open(os.path.join(PATH_DATA, 'va_train_seq_list.pickle'), 'wb') as f:
+                pickle.dump(train_data, f)
+
+
+        # Validation data
+        val_data = {
+            'x': [],
+            'y': [],
+            'i': []
+        }
+
+        val_subject_lists = read_csv(os.path.join(PATH_DATA, 'va_val_set.csv'))
+        for j, val_subject_list in enumerate(val_subject_lists):
+            val_images = read_csv(os.path.join(PATH_DATA_GUIDE, val_subject_list + '.csv'))
+
+            val_x, val_y, val_idx = get_sequence_data(val_subject_list, val_images, WINDOW_SIZE, STRIDE, switch_images, switch_subjects)
+
+            val_data['x'] += val_x
+            val_data['y'] += val_y
+            val_data['i'] += val_idx
+
+            print(j, len(val_subject_lists), np.array(val_data['x']).shape, np.array(val_data['y']).shape, np.array(val_x).shape)
+
+        val_data = filtering_topfull(val_data)
+
+        print(np.array(val_data['x']).shape, np.array(val_data['y']).shape)
+        if np.array(val_data['x']).shape[0] == np.array(val_data['y']).shape[0] :
+            with open(os.path.join(PATH_DATA, 'va_val_seq_list.pickle'), 'wb') as f:
+                pickle.dump(val_data, f)
 
 
 
